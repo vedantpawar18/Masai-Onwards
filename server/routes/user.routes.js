aac75471e2e5fc8cca20f2b8c350286c077965b5
vedantpawar18 <vedantpawar18@gmail.com>
2023-02-17 10:34:58 +0530

    signup api implementation without otp verification

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,12 +3,13 @@ const {UserModel} = require("../models/User.model")
 const userController = Router();
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-require("dotenv").config()
+require("dotenv").config();
+const fast2sms = require('fast-two-sms')
 
 userController.post("/signup", async(req, res) => {
     const {email, password, full_name,mob_numb} = req.body;
 
-    //  If user is signed up with email id 
+    //  <----------------------If user is signed up with email id --------------------------------------------------> //
 
     //  function to validate email id.
     function validateemail(mail)  
@@ -36,19 +37,20 @@ userController.post("/signup", async(req, res) => {
         }
       }
 
+
         if (email)
         {
-            const Alreadyuser = await UserModel.find({ email });
-            const validemail= validateemail(email);
-            const validname= validateUserName(full_name)
+            const alreadyUser = await UserModel.find({ email });
+            const valideMail= validateemail(email);
+            const validName= validateUserName(full_name)
 
-            if(validemail==false){
+            if(valideMail==false){
                 return res.status(401).send("Invalid email address !");
             }
-            else if (validname==false){
+            else if (validName==false){
                 return res.status(401).send("Please enter atleast 3 characters, no symbols or numbers as full name");
             }
-            else if (Alreadyuser.length>0){
+            else if (alreadyUser.length>0){
                 return res.status(403).send("User already exists");
             }
             else{
@@ -60,6 +62,87 @@ userController.post("/signup", async(req, res) => {
                 res.send("Signup successfull")   
             }
         }
+
+         //  <----------------------If user is signed up with mobile number--------------------------------------------------> //
+
+        if(mob_numb){
+
+          if(mob_numb.length==10){
+
+            function sendMessage(message,number,res) {
+              var options = {
+                authorization:
+                  "eYnsX3vdSZg9PrQFqIWz8RGVx4NkBAfKhJwu51jt7piL2mDUcEeLCUaKzx90Gl84Mo2Sq1cZ6OwfbdQB",
+                message:message,
+                numbers: [number],
+              };
+          
+              // send this message
+          
+              fast2sms
+                .sendMessage(options)
+                .then((response) => {
+                  console.log(response)
+                  res.send("SMS OTP Code Sent Successfully")
+                })
+                .catch((error) => {
+                  res.send("Some error taken place")
+                });
+          }
+          const message= "hello"
+          sendMessage(message,mob_numb,res)
+           
+          }
+          else {
+            return res.status(401).send("Invalid mobile number !");
+          }
+        }
+
+        //  <----------------------If user is signed up  with full name email or mobile number and password--------------------------------------------------> //
+
+        if(password && email){
+           const alreadyUser = await UserModel.find({ email });
+            const valideMail= validateemail(email);
+            const validName= validateUserName(full_name)
+
+            if(valideMail==false){
+                return res.status(401).send("Invalid email address !");
+            }
+            else if (validName==false){
+                return res.status(401).send("Please enter atleast 3 characters, no symbols or numbers as full name");
+            }
+            else if (alreadyUser.length>0){
+                return res.status(403).send("User already exists");
+            }
+            else if (password.length<8){
+                return res.status(401).send("password length should not be less than 8");
+            }
+            else{
+                const user = new UserModel({
+                    email,
+                    full_name,
+                    password
+                })
+                user.save()
+                res.send("Signup successfull")   
+            }
+        }
+
+        if(password && mob_numb){
+         
+          if(mob_numb==10){
+            const user = new UserModel({
+              mob_numb,
+              full_name,
+              password
+          })
+          user.save()
+          res.send("Signup successfull")  
+          } 
+          else {
+            return res.status(401).send("Invalid mobile number !");
+          }
+       }
         
 })
 
