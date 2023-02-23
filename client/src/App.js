afe978dfd67356a79d72394d143e5654c86ab9d0
ArunrajMK <arunrajshanker6@gmail.com>
2023-02-23 09:20:44 +0530

    Integrated signin api to ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
 
 import './App.css';
-import Model3 from './components/Model3';
-import Model2 from './components/Model2';
-import Model from './components/Model';
-import Navbar from './components/Navbar';
-import Model4 from './components/Medel4';
+
+
 
 import AllRoutes from './pages/AllRoutes';
 function App() {
